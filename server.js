976430bc5322fd78169acaf47d53bd05580e14e6
markolilu <marklumbpt@gmail.com>
2026-02-26 14:15:02 +0000

    added readData and writeData to server.js || next add the CRUDS! // also need to add the requirements.txt file, feel like that wiill be a good idea

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const {v4: uuidv4} = require('uuid');
 
 const app = express();
 
-const post = 3001;
+const PORT = 3001;
 
 app.use(express.json());
+
+const dataFilePath = path.join(__dirname, 'data.json');
+
+const readData = () => {
+    if (!fs.existsSync(dataFilePath)) {
+        return [];
+    }
+    const data = fs.readFileSync(dataFilePath);
+    return JSON.parse(data);
+};
+
+const writeData = (data) => {
+    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+};
+
+// write CRUDS below
